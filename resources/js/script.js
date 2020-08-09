5212667296befebbda89ffdebb8987260dfdbc7b
Maria P. Campos <mpfc.maria@gmail.com>
2020-08-09 18:41:51 -0300

    adding burguer menu for mobile

--- resources/js/script.js
+++ resources/js/script.js
@@ -48,6 +48,18 @@ $(document).ready(function() {
         offset:'50%'
     });
 
-
+    // Mobile nav
+    $('.js--nav-icon').click(function() {
+        let nav = $('.js--main-nav');
+        let icon = $('.js--nav-icon i');
+        nav.slideToggle(200);
+        if(icon.hasClass('ion-navicon-round')){
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round')
+        } else {
+            icon.removeClass('ion-close-round');
+            icon.addClass('ion-navicon-round')
+        }
+    });
 });
 
